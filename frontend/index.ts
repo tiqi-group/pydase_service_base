3b6a1d2d4f9ebab49267579b71aa4d9691733b7e
Mose Mueller <mosmuell@ethz.ch>
2023-04-11 08:47:45 +0200

    Updating frontend code

---
+++ frontend/index.ts
@@ -0,0 +1,7 @@
+export { ApiError } from './src/ApiError';
+export { ApiRequestOptions } from './src/ApiRequestOptions';
+export { ApiResult } from './src/ApiResult';
+export { CancelError, CancelablePromise, OnCancel } from './src/CancelablePromise';
+export { OpenAPI } from './src/OpenAPI';
+export type { OpenAPIConfig } from './src/OpenAPI';
+export { request } from './src/request';
